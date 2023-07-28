d1fd7d194a493836ea6d4a7ee1e25006660978fd
Pablo Arnalte-Mur <pablo.arnalte@gmail.com>
2023-07-28 20:30:45 +0200

    Re-organise input/lookup folders
    I re-organise the folders so that there is a clear separation between:
    - input parameter files (in input_model folder): these are the ones 
    defining the model to use (HOD, SMF, k-correction)
    - lookup files: these files are generated by the programme and saved to 
    skip costly calculations

--- hodpy/lookup.py
+++ hodpy/lookup.py
@@ -12,6 +12,14 @@ def get_lookup_dir():
     path[-1] = "lookup"
     return "/".join(path)
 
+def get_input_dir():
+    """
+    Returns the directory containing the input files
+    """
+    path = os.path.abspath(hodpy.__file__)
+    path = path.split("/")[:-1]
+    path[-1] = "input_model"
+    return "/".join(path)
 
 def read_hod_param_file(hod_param_file):
     """
@@ -26,19 +34,31 @@ def read_hod_param_file(hod_param_file):
     return Mmin_Ls, Mmin_Mt, Mmin_am, M1_Ls, M1_Mt, M1_am, M0_A, M0_B, \
             alpha_A, alpha_B, alpha_C, sigma_A, sigma_B, sigma_C, sigma_D
 
+## Input files
+# These have to be present *always*
+input_path = get_input_dir()
+
+# HOD parameters for BGS mock
+bgs_hod_parameters    = input_path+"/hod_params_miniJPAS.dat"
+
+# k-corrections
+kcorr_file = input_path+"/k_mass_corr_imag_z04.dat"
+
+# miniJPAS luminosity functions
+smf_params = input_path+"/smf_params.dat" # describe the miniJPAS catalogue.
+
+
+## Lookup files
+# These will be created if do not exist
+# Have to be removed when the model (defined by input files) is changed
 path = get_lookup_dir()
 
 # HOD parameters for BGS mock
-bgs_hod_parameters    = path+"/hod_params_miniJPAS.dat"
 bgs_hod_slide_factors = path+"/slide_factors.dat" # will be created if doesn't exist
 
 # lookup files for central/satellite magnitudes
 central_lookup_file   = path+"/central_stellmasses.npy"   # will be created if doesn't exist
 satellite_lookup_file = path+"/satellite_stellmasses.npy" # will be created if doesn't exist
 
-# k-corrections
-kcorr_file = path+"/k_mass_corr_imag_z04.dat"
-
 # miniJPAS luminosity functions
-smf_params      = path+"/smf_params.dat" # describe the miniJPAS catalogue.
 target_smf         = path+"/target_smf.dat" # will be created if doesn't exist
